aeb8d22e5a01f78b741b95fafb76742f5ece759e
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2021-04-19 23:17:58 -0500

    tercercommit

--- server.py
+++ server.py
@@ -2,27 +2,62 @@ import socketio
 
 from aiohttp import web
 
+data={}
+
 PORT=5000
 HOST='localhost'
 
-sio= socketio.AsyncServer(async_mode='aiohttp')
+sio= socketio.AsyncServer(async_mode='aiohttp',cors_allowed_origins='*')
 app=web.Application()
 sio.attach(app)
 
-@sio.event
-def connect(sid,environment):
-    print(sid,'connected')
 
 @sio.event
-def disconnect(sid):
+async def connect(sid,environment):
+
+    print(sid,'connected')
+@sio.event
+async def disconnect(sid):
     print(sid,'disconnected')
 
+@sio.event
+def  begin_chat(sid,roomname):
+    if roomname in data:
+        print('ya existe la sala')
+        sio.enter_room(sid,room=roomname)
+
+    else:
+        print('Creando nueva sala')
+        data[roomname]=[]
+        sio.enter_room(sid,room=roomname)
+@sio.event
+async def  exit_chat(sid,roomname):
+    sio.leave_room(sid,room=roomname)
+
+@sio.event
+async def  message(sid,data):
+    session= await sio.get_session(sid)
+    print(session['username'])
+    await sio.emit('newmsg',data,room='chat_users')
+
+
+@sio.event
+async def joinroom(sid,data):
+    print(sid+' '+data)
+    ar=data.split(',')
+    begin_chat()
 
 
 async def root(request):
     print(request.match_info.get('name',"Anonymous"))
     return web.Response(text='Hola Mundo')
 
+
+
+def auth(env):
+    paramsar=env['QUERY_STRING'].split('&')
+    paramsar=[p.split('=') for p in paramsar]
+    return paramsar[0][1]
 
 
 if __name__=='__main__':
--- static/script.js
+++ static/script.js
@@ -1,17 +1,89 @@
-const app = new Vue({
-    el:'chat'
-}) 
+const socket=io('http://localhost:5000/')
+socket.on('connect',()=>sessionStorage.setItem('sid',socket.id))
 
-function msgSound(){
-    new Audio('./assets/smsg.mp3').play()
+/*routes=[{path:'/chat',component:},{path:'/main',component:}]
+const router = new VueRouter({
+    routes
+})
+*/
 
-}
+Vue.component('rmsg',{
+    data:{
+        msj:'hola'
+    },
+    template:'<div class="rmsgbox"><div class="mensajer">{{ msj }}</div></div>'
+    
+})
 
-function enterMsg(e) {
+Vue.component('smsg',{
+    data:{
+        msj:'hola'
+    },
+    template:'<div class="smsgbox"><div class="mensajes">{{ msj }}</div></div>',
+    
+})
 
-    console.log(e)
-    if (e.key=='Enter'){
-        document.getElementById('msgButton').click()
-        document.getElementById('msgInput').value=''
+new Vue({
+    el:'#chatbox',
+    created(){
+        socket.on('newmsg',(msg)=>{
+            this.messages.push({'msg':msg,name:''})
+        })
+    },
+    data:{
+        messages:[]
+    },
+    methods:{
     }
-}
+})
+
+new Vue({
+    el:'#msgbox',
+    data:{
+        message:''
+    },
+    methods:{
+        sendmsg(){
+            socket.emit('message',this.message)
+            this.message=''
+        },
+        msgSound(){
+            new Audio('./assets/smsg.mp3').play()
+        }
+    }
+})
+
+new Vue({
+    el:'#joinroomform',
+    data:{
+        inroom:false,
+        username:'',
+        roomcode:'',
+        maxl:6
+    },
+    methods:{
+        joinroom(){
+            this.inroom=true
+            socket.emit('joinroom',this.username+','+this.roomcode)
+        },
+        leaveroom(){
+            this.inroom=false
+        },
+        upprc(){
+            this.roomcode=this.roomcode.toUpperCase()
+            if(this.roomcode.length>this.maxl){
+                this.roomcode=this.roomcode.substring(0,this.maxl)
+            }
+        }
+    },
+    filters:{
+        capitalize: function (value) {
+            if (!value) return ''
+            value = value.toString()
+            return value.toUpperCase()
+          }
+    }
+})
+
+
+
